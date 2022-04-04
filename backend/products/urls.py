2701ebb1255692a60486d08c5edd5c46ca6bbc9f
krishnapm642 <krishnapm642@gmail.com>
2022-04-04 22:41:57 +0530

    product mixin view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,11 +1,10 @@
-from unicodedata import name
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail-view'),
-    path('', views.ProductListCreateAPIView.as_view()),
+    path('<int:pk>/', views.product_mixin_view),
+    path('', views.product_mixin_view),
     path('<int:pk>/update', views.ProductUpdateAPIView.as_view()),
     path('<int:pk>/delete', views.ProductDeleteAPIView.as_view())
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .serializers import ProductSerializer
 from .models import Product
 
 # Create your views here.
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
+
+product_mixin_view = ProductMixinView.as_view()
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
